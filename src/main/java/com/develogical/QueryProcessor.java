874fa2364f85202bf732cbaa23c56fcad3255589
Kian Nassre <knassre@macbook-pro-403.wifi.local.cmu.edu>
2022-09-22 15:40:49 -0400

    Added 'largest number' query

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,14 +3,24 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        String lowerVerison = query.toLowerCase();
-        if (lowerVerison.contains("shakespeare")) {
+        String lowerVersion = query.toLowerCase();
+        if (lowerVersion.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (lowerVerison.contains("what is your name")) {
+        if (lowerVersion.contains("what is your name")) {
             return "teedy-is-tedious";
+        }
+
+        if (lowerVersion.contains("which of the following numbers is the largest:")) {
+            String suffix = lowerVersion.split(":")[1];
+            Integer best = 0;
+            for (String x : suffix.split(",")) {
+                Integer num = Integer.parseInt(x);
+                best = Integer.max(best, num);
+            }
+            return String.valueOf(best);
         }
         return "";
     }
