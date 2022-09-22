e254a66379ef6276e9f854135e5175d7ac702fe0
Kian Nassre <knassre@macbook-pro-393.myfiosgateway.com>
2022-09-22 00:18:19 -0400

    Pushing bad test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -25,4 +25,9 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("shakespeare"), containsString("playwright"));
     }
 
+    @Test
+    public void badTest() throws Exception {
+        assertThat(queryProcessor.process("hello"), containsString("goodbye"));
+    }
+
 }
